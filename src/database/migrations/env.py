929fb767749f22928618bf207ebaddbb830ce575
ValentinDobrynin <v.dobrynin@gmail.com>
2025-04-04 12:24:04 +0300

    fix: use database URL from settings in env.py

--- src/database/migrations/env.py
+++ src/database/migrations/env.py
@@ -15,6 +15,9 @@ from src.database.models import *  # Import all models
 # access to the values within the .ini file in use.
 config = context.config
 
+# Override sqlalchemy.url with the one from settings
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
