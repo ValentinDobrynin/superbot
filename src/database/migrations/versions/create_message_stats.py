a31fac59cdbe9799cc99a160ca0cb6976a00c997
ValentinDobrynin <v.dobrynin@gmail.com>
2025-04-04 23:23:54 +0300

    Fix: Update migration dependencies and remove duplicate chat table creation

--- src/database/migrations/versions/create_message_stats.py
+++ src/database/migrations/versions/create_message_stats.py
@@ -1,7 +1,7 @@
 """create message_stats table
 
 Revision ID: create_message_stats
-Revises: create_chat_stats
+Revises: 20250404_1140_9606bc129069_remove_is_active_column_from_chats_table
 Create Date: 2024-04-04 19:35:00.000000
 
 """
@@ -13,7 +13,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision: str = 'create_message_stats'
-down_revision: Union[str, None] = 'create_chat_stats'
+down_revision: Union[str, None] = '20250404_1140_9606bc129069_remove_is_active_column_from_chats_table'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
