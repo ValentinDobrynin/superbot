1ff66806c00551ac443b7db6d05b36df03535bbc
ValentinDobrynin <v.dobrynin@gmail.com>
2025-04-04 12:42:39 +0300

    fix: update imports in env.py to remove src prefix

--- src/database/migrations/env.py
+++ src/database/migrations/env.py
@@ -7,9 +7,9 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
-from src.config import settings
-from src.database.base import Base
-from src.database.models import *  # Import all models
+from config import settings
+from database.base import Base
+from database.models import *  # Import all models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
