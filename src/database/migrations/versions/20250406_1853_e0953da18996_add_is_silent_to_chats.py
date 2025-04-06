b78d49395b63da93a3a4670901b8430d9f564278
ValentinDobrynin <v.dobrynin@gmail.com>
2025-04-06 22:00:56 +0300

    Add migrations for is_silent field and align database with models

---
+++ src/database/migrations/versions/20250406_1853_e0953da18996_add_is_silent_to_chats.py
@@ -0,0 +1,28 @@
+"""add_is_silent_to_chats
+
+Revision ID: e0953da18996
+Revises: 9e1f012a388e
+Create Date: 2024-04-06 18:53:00.000000
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'e0953da18996'
+down_revision: Union[str, None] = '9e1f012a388e'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    # Add is_silent column to chats table
+    op.add_column('chats', sa.Column('is_silent', sa.Boolean(), nullable=False, server_default='true'))
+
+
+def downgrade() -> None:
+    # Remove is_silent column from chats table
+    op.drop_column('chats', 'is_silent') 
