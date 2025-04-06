7eec65285b0c6bfa664348cc6966846df9567d9e
ValentinDobrynin <v.dobrynin@gmail.com>
2025-04-06 23:29:53 +0300

    Add response_probability and importance_threshold columns to chats table

---
+++ src/database/migrations/versions/20250406_2029_add_response_probability_to_chats.py
@@ -0,0 +1,30 @@
+"""add_response_probability_to_chats
+
+Revision ID: 20250406_2029
+Revises: 998bd13245d6
+Create Date: 2024-04-06 20:29:00.000000
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '20250406_2029'
+down_revision: Union[str, None] = '998bd13245d6'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    # Add response_probability and importance_threshold columns to chats table
+    op.add_column('chats', sa.Column('response_probability', sa.Float(), nullable=False, server_default='1.0'))
+    op.add_column('chats', sa.Column('importance_threshold', sa.Float(), nullable=False, server_default='0.5'))
+
+
+def downgrade() -> None:
+    # Remove response_probability and importance_threshold columns from chats table
+    op.drop_column('chats', 'response_probability')
+    op.drop_column('chats', 'importance_threshold') 
